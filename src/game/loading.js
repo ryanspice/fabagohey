112cc24fda931281fb1513ac590421b60905181e
Ryan Spice <spice.ryan@hotmail.com>
2018-01-15 00:33:42 -0500

    Commenting, refactoring, priority and preallocating in loading.js Moving Array.clean to utils.js

--- src/game/loading.js
+++ src/game/loading.js
@@ -23,26 +23,12 @@ declare var require:any;
 
 let lastError = 0;
 
-Array.prototype.clean = function(deleteValue) {
-  for (var i = 0; i < this.length; i++) {
-    if (this[i] == deleteValue) {
-      this.splice(i, 1);
-      i--;
-    }
-  }
-  return this;
-};
-
-
 /* Loading state */
 
 class Loading extends NewState {
 
 	static asyncDoneLoading:boolean = false;
-	static getErrors(){
 
-
-	}
 	/* Pass self into Sprite for secure inheritence ( SS ) */
 
 	constructor(){
@@ -55,84 +41,97 @@ class Loading extends NewState {
 
 	static async init():Promise<void> {
 
+		//TODO: build into SpiceJS
 		this.asyncDoneLoading = false;
 
-		//Set buffer index of the UI draw event TODO: ?
-		this.visuals.bufferIndex = 0;
+		//TODO: build into SpiceJS
+		this.app.client.loader.graphics = await this.graphics;
 
 		//region Override Visuals
-
 		//TODO: bring to SpiceJS.... overrides sort method specifically for priority
-		this.visuals.PriorityRegistryAttempt = 64;
+		//	visuals.PriorityRegistryAttempt - size of array on initial load
+		//	visuals.PriorityRegistryAttemptCount - count of time accessed array
+		//	visuals.PriorityRegistry - array of all data (game objects)
+
+		//	visuals.sortPriorityRegistry - sort radix if sorting
+		//	visuals.appendNew - now caches array then cleans remaining, will warn if adding more to cache.
+		//	visuals.PrioritySort - now uses radixSort
+		//
+
+		this.visuals.PriorityRegistryAttempt = 96;
 		this.visuals.PriorityRegistryAttemptCount = 0;
-		this.visuals.PriorityRegistry = new Array(64);
+		this.visuals.PriorityRegistry = new Array(this.visuals.PriorityRegistryAttempt);
+
+		/* sort using radix if sorting */
 
 		this.visuals.sortPriorityRegistry =	function():void {
 
-			if (this.PriorityRegistryFlags.sort === true) {
-				this.PriorityRegistry.radixSort(); // = sortBy(this.PriorityRegistry,(o)=>{return -o.priority;});
-				this.PriorityRegistryFlags.sort = false;
-			}
+			if (!this.PriorityRegistryFlags.sort === true)
+				return;
+
+			this.PriorityRegistry.radixSort(); // = sortBy(this.PriorityRegistry,(o)=>{return -o.priority;});
+			this.PriorityRegistryFlags.sort = false;
 
 		};
 
-		this.asyncDoneLoading = false;
-
-		//TODO: fix inside SpiceJS
-		this.app.client.loader.graphics = await this.graphics;
+		/* caches array then cleans remaining, will warn if adding more to cache.  */
 
 		this.visuals.appendNew = function(toRegister:any){
 
 			if (!this.PriorityRegistry){
-
 				this.PriorityRegistry = new Array(this.PriorityRegistryAttempt);
-
 			}
 
-				//if (this.PriorityRegistryAttemptCount>64)
-				//return this.PriorityRegistry.push(toRegister);
-				//else {
+			if (this.PriorityRegistryAttemptCount>this.PriorityRegistryAttempt){
 
-					this.PriorityRegistry[this.PriorityRegistryAttemptCount] = toRegister;
-					this.PriorityRegistryAttemptCount++;
-					this.PriorityRegistry = this.PriorityRegistry.clean(undefined);
-					return true;
-				//}
+				console.warn('SJS:Visuals:PriorityRegistry:'+this.PriorityRegistryAttempt+': Pushing at ' + this.PriorityRegistryAttemptCount++);
 
+				return this.PriorityRegistry.push(toRegister);
+			} else {
+
+				this.PriorityRegistry[this.PriorityRegistryAttemptCount] = toRegister;
+				this.PriorityRegistry = this.PriorityRegistry.clean(undefined); //TODO move into just before sort proprly
+				this.PriorityRegistryAttemptCount++;
+
+				return true;
+			}
 
 		}
 
+		/* using radixSort */
+
 		this.visuals.PrioirtySort = function(){
-			//let t = this.PriorityRegistry.radixSort();
-			//console.log(this.PriorityRegistry);
+
+			this.PriorityRegistry.radixSort();
+
 			return this.PriorityRegistry;
 		}
 
+		//
 		//endregion
 
-
-		this.spinner =  new Spinner(this.visuals,1);
+		this.spinner =  new Spinner(this.visuals);
 		this.spinner.colour = this.spinner.getColour('Red');
+
 		//Load spriteDataList from data folder TODO: add to SpiceJS as API - look into dynamic? nawh
 		/*this.spriteDataList = await require.ensure(['../require/data'],async ()=>{
 
 			return await require('../require/data').default.spriteDataList;
 		},'maps');
 		*/
-
 		this.spriteDataList = await require.ensure(['../require/data'],async ()=> await require('../require/data').default.spriteDataList,'maps');
 
 		//Load sprites
-		for(let i = 0; i<=this.spriteDataList.length-1;i++){
+		let i = this.spriteDataList.length-1;
+		for(i; i>=0;i--){
+
+			//let s = ;
 			await this.app.client.loader.asyncLoadImage(this.spriteDataList[i],String("spr"+i));
 		}
 
 		//Build BackgroundController
 		this.BackgroundManager  = await new BackgroundController(new StatsBuffer('',0,0,1,1,0,0,0,272,160),this.visuals);
 
-		this.asyncDoneLoading = true;
-
-		this.app.Loading = this;
 
 		this.gotoGame = ()=>{
 
@@ -148,6 +147,10 @@ class Loading extends NewState {
 
 		this.gamepad =  this.visuals.app.input.gamepads;
 
+		this.asyncDoneLoading = true;
+		this.app.Loading = this;
+
+		//TODO: move to spicejs
 		await this.visuals.PrioirtySort();
 	}
 
@@ -161,10 +164,12 @@ class Loading extends NewState {
 		//			we need to check the error has changed
 		let _continue = false;
 		let _errors = this.app.client.graphics.getErrors();
+
 		//if no errors, continue is true
 		if (_errors===0) {
 			_continue = true;
 		}
+
 		//if errors equals last error, cease to continue
 		if (_errors===lastError) {
 
@@ -204,7 +209,6 @@ class Loading extends NewState {
 			}
 
 		}
-
 
 	};
 
--- src/game/utils.js
+++ src/game/utils.js
@@ -1,5 +1,15 @@
 //@flow
 
+Array.prototype.clean = function(deleteValue) {
+  for (var i = 0; i < this.length; i++) {
+    if (this[i] == deleteValue) {
+      this.splice(i, 1);
+      i--;
+    }
+  }
+  return this;
+};
+
 import type {
 	dtoBatchDataValidation
 } from './core/interfaces';
